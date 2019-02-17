4618330998df476a3c2c820ddfcc5fcd11c9ed72
Hazel Wright <hazel.wright@live.co.uk>
2019-02-17 20:45:45 +0000

    create default collections

---
+++ src/scripts/create_defaults.py
@@ -0,0 +1,22 @@
+from models.collections import Collection
+from constants import collection_types
+
+
+def create_default_collections():
+    try:
+        db.session.add(
+            Collection(name='Index', type=collection_types.INDEX)
+        )
+        db.session.add(
+            Collection(name='Future Log', type=collection_types.FUTURE)
+        )
+        db.session.add(
+            Collection(name='Monthly Log', type=collection_types.MONTHLY)
+        )
+        db.session.add(
+            Collection(name='Daily Log', type=collection_types.DAILY)
+        )
+        db.session.commit()
+    except:
+        print("Unable to add default collections to DB")
+        db.session.rollback()
